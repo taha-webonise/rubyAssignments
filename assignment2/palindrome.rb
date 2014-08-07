2e623ee00227cfbb8a427577ed60bb868d14fb37
Taha Husain <taha.husain@weboniselab.com>
2014-08-07 13:52:32 +0530

    palindrome question: Done

---
+++ assignment2/palindrome.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+class Palindrome
+	# Funtion to find if a string is palindrome or not regardless of cases and whitespaces
+	def palindrome(str)
+		if str.delete(" ").casecmp(str.delete(" ").reverse) == 0
+			print "String is a palindrome!\n"
+		else
+			print "String is not a palindrome\n"
+		end
+	end
+end
+
+print "Please enter the string you want to check for palindrome: "
+str = ($stdin.gets.chomp)
+pal = Palindrome.new
+pal.palindrome(str)
